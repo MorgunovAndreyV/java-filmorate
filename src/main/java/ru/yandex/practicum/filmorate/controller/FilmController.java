4400e9c729659ce6fcf3260531a3f749cc545029
unknown <lobdog@yandex.ru>
2023-11-04 11:46:42 +0300

    feat.Первичная рабочая реализация.

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -0,0 +1,104 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.FilmControllerException;
+import ru.yandex.practicum.filmorate.model.Film;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/films")
+@Slf4j
+public class FilmController {
+    private Collection<Film> films = new HashSet<>();
+    @Getter
+    private final LocalDate lowThreshholdDate = LocalDate.parse("28.12.1895", DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    private final int FILM_DESCRIPTION_LENGTH = 200;
+
+    @GetMapping
+    public Collection<Film> getAll() {
+        return films;
+    }
+
+    @PostMapping
+    public Film addNew(@RequestBody Film film) throws FilmControllerException {
+        filmValidations(film);
+
+        if (films.contains(film)) {
+            throw new FilmControllerException("Такой фильм уже добавлен");
+        }
+        assignNewId(film);
+        films.add(film);
+        log.info("Новый фильм добавлен успешно. id:" + film.getId());
+
+        return film;
+    }
+
+    @PutMapping
+    public Film change(@RequestBody Film film) throws FilmControllerException {
+        Film filmFromBase = getFilmByIdString(film.getId());
+
+        filmValidations(film);
+        filmFromBase.setName(film.getName());
+        filmFromBase.setDescription(film.getDescription());
+        filmFromBase.setReleaseDate(film.getReleaseDate());
+        filmFromBase.setDuration(film.getDuration());
+
+        log.info("Запись фильма изменена успешно. id:" + film.getId());
+
+        return filmFromBase;
+    }
+
+    private void filmValidations(Film film) throws FilmControllerException {
+        if (film.getName() == null || film.getName().isEmpty()) {
+            throw new FilmControllerException("Название фильма не может быть пустым");
+        }
+
+        if (film.getDescription() != null) {
+            if (film.getDescription().length() >= FILM_DESCRIPTION_LENGTH) {
+                throw new FilmControllerException("Описание фильма не может быть длиннее 200 символов");
+            }
+
+        }
+
+        if (film.getReleaseDate() != null) {
+            if (film.getReleaseDate().isBefore(lowThreshholdDate)) {
+                throw new FilmControllerException("Дата релиза не может быть раньше " + lowThreshholdDate.toString());
+            }
+
+        }
+
+        if (film.getDuration() != null) {
+            if (film.getDuration() < 0) {
+                throw new FilmControllerException("Продолжительность фильма не может быть отрицательной");
+            }
+
+        }
+
+    }
+
+    private void assignNewId(Film film) {
+        if (films != null) {
+            film.setId((long) (films.size() + 1));
+        }
+
+    }
+
+    private Film getFilmByIdString(Long id) throws FilmControllerException {
+        Optional<Film> possibleFilm = films.stream()
+                .filter(film1 -> film1.getId().equals(id))
+                .findFirst();
+
+        if (possibleFilm.isEmpty()) {
+            throw new FilmControllerException("Фильм с ID " + id + " не найден.");
+        } else return possibleFilm.get();
+
+    }
+
+}
---
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -0,0 +1,254 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.yandex.practicum.filmorate.exception.UserControllerException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+class UserControllerTest {
+    private UserController userController;
+    private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+    private User user;
+    private User existingUser;
+    private Long existingUserId;
+
+    @BeforeEach
+    void setUp() {
+        userController = new UserController();
+        user = User.builder()
+                .email("mail@mail.ru")
+                .login("dolore")
+                .name("Nick Name")
+                .birthday(LocalDate.parse("1946-08-20", dateTimeFormatter))
+                .build();
+
+        existingUser = User.builder()
+                .email("312mail@mail.ru")
+                .login("instantCoffeer")
+                .name("Coco Chanelle")
+                .birthday(LocalDate.parse("1966-08-20", dateTimeFormatter))
+                .build();
+
+        userController.addNew(existingUser);
+        existingUserId = userController.getAll()
+                .stream().filter(user1 -> user1.equals(existingUser)).findFirst().get().getId();
+
+    }
+
+    @Test
+    void checkaddNewValidUser() {
+        userController.addNew(user);
+        Assertions.assertTrue(userController.getAll().contains(user));
+    }
+
+    @Test
+    void checkaddUserWithEmptyLogin() {
+        user.setLogin("");
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Логин не может быть пустым", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+        user.setLogin(null);
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Логин не может быть пустым", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+        user.setLogin("Login with whitespaces");
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Логин не может содержать пробелы", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+    }
+
+    @Test
+    void checkaddUserWithWrongEmail() {
+        user.setEmail(null);
+
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Почта не может быть пустой", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+        user.setEmail("");
+
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Почта не может быть пустой", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+        user.setEmail("asd");
+
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+        Assertions.assertEquals("Некорректный формат почты", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+
+    }
+
+    @Test
+    void checkaddUserWithWrongBirthdate() {
+        user.setBirthday(LocalDate.parse("2099-02-01", dateTimeFormatter));
+
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.addNew(user));
+
+        Assertions.assertEquals("Дата рождения не может быть в будущем", exception.getMessage());
+        Assertions.assertFalse(userController.getAll().contains(user));
+    }
+
+    @Test
+    void checkaddUserWithEmptyName() {
+        user.setName("");
+        userController.addNew(user);
+        Assertions.assertEquals(user.getName(), user.getLogin());
+        Assertions.assertTrue(userController.getAll().contains(user));
+    }
+
+    @Test
+    void checkModifyUserCorrectly() {
+        String newName = "NewName1";
+        String newLogin = "NewLogin";
+        String newEmail = "newemail@mail.com";
+        LocalDate newBirthDate = LocalDate.parse("1990-12-02", dateTimeFormatter);
+
+        userController.change(User.builder()
+                .id(existingUserId)
+                .email(newEmail)
+                .login(newLogin)
+                .name(newName)
+                .birthday(newBirthDate)
+                .build()
+        );
+
+        Assertions.assertTrue(newName.equals(existingUser.getName()) &&
+                newLogin.equals(existingUser.getLogin()) &&
+                newEmail.equals(existingUser.getEmail()) &&
+                newBirthDate.equals(existingUser.getBirthday()));
+    }
+
+    @Test
+    void checkModifyLoginWrongly() {
+        final String finalNewLogin = "";
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(existingUser.getEmail())
+                        .login(finalNewLogin)
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Логин не может быть пустым", exception.getMessage());
+        Assertions.assertNotEquals(finalNewLogin, existingUser.getLogin());
+
+        final String finalNewLogin2 = null;
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(existingUser.getEmail())
+                        .login(finalNewLogin2)
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Логин не может быть пустым", exception.getMessage());
+        Assertions.assertNotEquals(finalNewLogin2, existingUser.getLogin());
+
+        final String finalNewLogin3 = "Login whitespace";
+
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(existingUser.getEmail())
+                        .login(finalNewLogin3)
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Логин не может содержать пробелы", exception.getMessage());
+        Assertions.assertNotEquals(finalNewLogin3, existingUser.getLogin());
+
+    }
+
+    @Test
+    void checkModifyUserWithWrongBirthdate() {
+        final LocalDate finalDate = LocalDate.parse("2099-02-01", dateTimeFormatter);
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(existingUser.getEmail())
+                        .login(existingUser.getLogin())
+                        .name(existingUser.getName())
+                        .birthday(finalDate)
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Дата рождения не может быть в будущем", exception.getMessage());
+        Assertions.assertNotEquals(finalDate, existingUser.getBirthday());
+
+    }
+
+    @Test
+    void checkModifyUserWithWrongEmail() {
+        final String newFinalEmail = "";
+        Exception exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(newFinalEmail)
+                        .login(existingUser.getLogin())
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Почта не может быть пустой", exception.getMessage());
+        Assertions.assertNotEquals(newFinalEmail, existingUser.getBirthday());
+
+        final String newFinalEmail2 = null;
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(newFinalEmail2)
+                        .login(existingUser.getLogin())
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Почта не может быть пустой", exception.getMessage());
+        Assertions.assertNotEquals(newFinalEmail2, existingUser.getBirthday());
+
+        final String newFinalEmail3 = "email.com";
+        exception = Assertions.assertThrows(UserControllerException.class,
+                () -> userController.change(User.builder()
+                        .id(existingUserId)
+                        .email(newFinalEmail3)
+                        .login(existingUser.getLogin())
+                        .name(existingUser.getName())
+                        .birthday(existingUser.getBirthday())
+                        .build()
+                )
+        );
+
+        Assertions.assertEquals("Некорректный формат почты", exception.getMessage());
+        Assertions.assertNotEquals(newFinalEmail3, existingUser.getBirthday());
+
+    }
+
+}
